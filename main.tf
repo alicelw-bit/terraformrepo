c660b9c7f8eedb8968777064098727e79f71845f
alicelw-bit <alicellw@outlook.com>
2026-01-30 14:38:45 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "RG1"
+    resource_group_name  = "RG4"
     storage_account_name = "jenkinsstatealice"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
@@ -25,6 +25,7 @@ resource "azurerm_resource_group" "example" {
   name     = "RG2"
   location = "westeurope"
 }
+
 
 
 
